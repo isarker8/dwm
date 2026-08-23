a29d4be780bcfc1c9d4a79a0f5bf875f590a00e8
Ibrahim Sarker <168856336+isarker8@users.noreply.github.com>
2026-08-23 08:18:37 +0100

    adding removing capabilities

--- config.h
+++ config.h
@@ -125,7 +125,8 @@ static const Key keys[] = {
     { MODKEY|ShiftMask,             XK_l,      spawn,          SHCMD("slock") }, 
     { MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("screenshot") },
     { MODKEY|ShiftMask,             XK_a,      spawn,          SHCMD("audio") },
-    { MODKEY|ShiftMask,             XK_v,      spawn,          SHCMD("clipmenu -b -l 5 -i -p 'Clipboard:'") },    /* Select from clipmenu then paste */
+    { MODKEY|ShiftMask,             XK_v,      spawn,          SHCMD("clipmenu -b -l 5 -i -p 'Clipboard:'") },    /* Select from clipmenu add to primary */
+    { MODKEY|ShiftMask,             XK_x,      spawn,          SHCMD("clipdelmenu -b -l 5 -i -p 'Delete Entry:'") },
     { MODKEY|ShiftMask,             XK_BackSpace, spawn,       SHCMD("toggle-layout") },    
 };
 
